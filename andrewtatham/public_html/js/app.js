c5ff7b1425212f664dffe496cb7c0c3917bc1a3c
Andrew Tatham <andrewtatham@gmail.com>
2016-10-30 16:38:24 +0000

    James

--- andrewtatham/public_html/js/app.js
+++ andrewtatham/public_html/js/app.js
@@ -1,1 +1,20 @@
-﻿
+﻿function jamesAge() {
+    var from = new Date(2016, 4, 5, 10, 44);
+    var to = new Date();
+    var age = to - from;
+    var days = age / (1000 * 60 * 60 * 24);
+    var weeks = age / (1000 * 60 * 60 * 24 * 7);
+    var months = age / (1000 * 60 * 60 * 24 * 365.25 / 12);
+    var years = age / (1000 * 60 * 60 * 24 * 365.25);
+    // document.getElementById("to").innerHTML = to;
+    // document.getElementById("from").innerHTML = from;
+    document.getElementById("days").innerHTML = days.toFixed(6);
+    document.getElementById("weeks").innerHTML = weeks.toFixed(6);
+    document.getElementById("months").innerHTML = months.toFixed(6);
+    document.getElementById("years").innerHTML = years.toFixed(6);
+}
+
+function onload() {
+    var myVar = setInterval(jamesAge, 1000);
+    jamesAge();
+}
