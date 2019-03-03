e73e2203e08dc1880e892f97171f460e4d5d21ed
Andrew Tatham <andrewtatham@gmail.com>
2019-03-03 21:57:40 +0000

    random facts

--- public/js/app.js
+++ public/js/app.js
@@ -6,17 +6,25 @@ function jamesAge() {
     var weeks = age / (1000 * 60 * 60 * 24 * 7);
     var months = age / (1000 * 60 * 60 * 24 * 365.25 / 12);
     var years = age / (1000 * 60 * 60 * 24 * 365.25);
-    // document.getElementById("to").innerHTML = to;
-    // document.getElementById("from").innerHTML = from;
+
     document.getElementById("days").innerHTML = days.toFixed(6);
     document.getElementById("weeks").innerHTML = weeks.toFixed(6);
     document.getElementById("months").innerHTML = months.toFixed(6);
     document.getElementById("years").innerHTML = years.toFixed(6);
 }
 
+function loadRandomFact() {
+    var facts = document.getElementsByClassName("fact");
+    var index = Math.floor(Math.random() * facts.length)
+    document.getElementById("random-fact").innerHTML = facts[index].outerHTML;
+}
+
 function onload() {
-    var myVar = setInterval(jamesAge, 1000);
+    var timer1 = setInterval(jamesAge, 1000);
     jamesAge();
+
+    var timer2 = setInterval(loadRandomFact, 10000);
+    loadRandomFact();
 
     particlesJS.load('particles-js', '/js/particlesjs-config.json');
 }
