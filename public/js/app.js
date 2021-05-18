2a4d1ac9a6dc8aaa3ddeb7dfe16fa59cc52e807c
andrewtatham <andrewtatham@gmail.com>
2021-05-18 10:09:28 +0100

    emily

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,24 @@
-function jamesAge() {
-    var from = new Date(2016, 4, 5, 10, 44);
+function updateAges() {
     var to = new Date();
+    updateAge(new Date(2016, 4, 5, 10, 44), to, "james");
+    updateAge(new Date(2021, 4, 16, 18, 10), to, "emily");
+}
+
+function updateAge(from, to, prefix) {
     var age = to - from;
     var days = age / (1000 * 60 * 60 * 24);
     var weeks = age / (1000 * 60 * 60 * 24 * 7);
     var months = age / (1000 * 60 * 60 * 24 * 365.25 / 12);
     var years = age / (1000 * 60 * 60 * 24 * 365.25);
-    // document.getElementById("to").innerHTML = to;
-    // document.getElementById("from").innerHTML = from;
-    document.getElementById("days").innerHTML = days.toFixed(6);
-    document.getElementById("weeks").innerHTML = weeks.toFixed(6);
-    document.getElementById("months").innerHTML = months.toFixed(6);
-    document.getElementById("years").innerHTML = years.toFixed(6);
+    document.getElementById(`${prefix}-days`).innerHTML = days.toFixed(6);
+    document.getElementById(`${prefix}-weeks`).innerHTML = weeks.toFixed(6);
+    document.getElementById(`${prefix}-months`).innerHTML = months.toFixed(6);
+    document.getElementById(`${prefix}-years`).innerHTML = years.toFixed(6);
 }
 
 function onload() {
-    var myVar = setInterval(jamesAge, 1000);
-    jamesAge();
-
+    var myVar = setInterval(updateAges, 1000);
+    updateAges();
     particlesJS.load('particles-js', '/js/particlesjs-config.json');
 }
 
